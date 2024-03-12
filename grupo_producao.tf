7b0a5dda241efabc34c200b7366b73144cdc9969
cleuton-lopes <cleuton.lopes@gmail.com>
2024-03-12 10:21:53 -0300

    7d594f92-e073-11ee-b0fb-0c96e6fb153f

--- grupo_producao.tf
+++ grupo_producao.tf
@@ -1,11 +1,6 @@
 resource "estaleiro_grupo_seguranca" "grupo-01-prod" {
     nome = "grupo-01-prod"
     allowed {
-        cidr = "2801:80:3200::/40"
-        descricao = " Orgão 54"
-
-    }
-    allowed {
         cidr = "2804:301:4001::200/122"
         descricao = " Orgão 54"
 
