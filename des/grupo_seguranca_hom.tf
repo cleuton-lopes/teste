377b332bf96242c7ba2c5861912136979cd5594d
cleuton-lopes <cleuton.lopes@gmail.com>
2025-02-07 12:28:00 -0300

    dd0b4431-e567-11ef-bb6d-0c96e6fb153f

--- des/grupo_seguranca_hom.tf
+++ des/grupo_seguranca_hom.tf
@@ -1,13 +1,48 @@
 resource "estaleiro_grupo_seguranca" "apigateway-iac-hom-des" {
     nome = "apigateway-iac-hom-des"
     allowed {
+        cidr = "10.200.145.4/32"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "10.200.145.9/32"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "10.200.146.1/32"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "10.200.146.2/32"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "2001:12f0:c06::/48"
+        descricao = ""
+
+    }
+    allowed {
         cidr = "2801:80:3200::/40"
-        descricao = " Orgão 54"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "2804:301:4000::/126"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "2804:301:4001::200/122"
+        descricao = ""
 
     }
     allowed {
         cidr = "2804:594::/32"
-        descricao = " Orgão 54"
+        descricao = ""
 
     }
 }
--- des/grupo_seguranca_prod.tf
+++ des/grupo_seguranca_prod.tf
@@ -1,8 +1,48 @@
 resource "estaleiro_grupo_seguranca" "apigateway-iac-prod-des" {
     nome = "apigateway-iac-prod-des"
     allowed {
+        cidr = "10.200.145.9/32"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "10.200.146.1/32"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "10.200.146.2/31"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "177.69.144.40/29"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "200.148.207.0/24"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "2001:4860::/27"
+        descricao = ""
+
+    }
+    allowed {
         cidr = "2801:80:3200::/40"
-        descricao = " Orgão 54"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "2804:301:4001::200/122"
+        descricao = ""
+
+    }
+    allowed {
+        cidr = "2804:594::/32"
+        descricao = ""
 
     }
 }
